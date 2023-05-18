6d58122b5c53d15f93d292eaf99fb35558fd3e23
Francesca Galletti <francesca.galletti@pipefy.com>
2023-05-18 15:18:27 -0300

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import React from "react";
-import { Button } from "./components/Button";
+import { Route } from "./components/Route";
+import { Sidebar } from "./components/Sidebar";
+import {AccordionPage} from "./pages/AccordionPage";
+import {ButtonPage} from "./pages/ButtonPage";
+import {DropdownPage} from "./pages/DropdownPage"
+import { ModalPage } from "./pages/ModalPage";
 
 export const App = () => {
   return (
-    <div>
-      <div>
-        <Button>Primary</Button>
-      </div>
-      <div>
-        <Button>Secondary</Button>
-      </div>
-      <div>
-        <Button>Warning</Button>
-      </div>
-      <div>
-        <Button>Danger</Button>
-      </div>
-      <div>
-        <Button>Success</Button>
-      </div>
-      <div>
-        <Button>Plain</Button>
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+    <Sidebar />
+      <div className="col-span-5">
+      <Route path="/">
+          <DropdownPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
+        </Route>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/modal">
+          <ModalPage />
+        </Route>
       </div>
     </div>
-  )
-}
+  );
+};
