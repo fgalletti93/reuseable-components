47d05e17df6a8c91ea6ac18805b4a4a72b2edfb6
Francesca Galletti <francesca.galletti@pipefy.com>
2023-05-18 15:17:09 -0300

    creating button page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -11,7 +11,6 @@ export const Button = ({
   danger,
   ...rest
 }) => {
-  console.log(rest)
   const classes = className(rest.className, "flex items-center px-3 py-1.5 border", {
     "border-blue-500 bg-blue-500 text-white": primary,
     "border-gray-900 bg-gray-900 text-white": secondary,
