09f8db904db9ce6ced6b90b77c617bbc89f61677
Francesca Galletti <francesca.galletti@pipefy.com>
2023-05-20 18:20:59 -0300

    creating modal with react portal

--- src/pages/ModalPage.jsx
+++ src/pages/ModalPage.jsx
@@ -1,7 +1,203 @@
-import { Modal } from "../components/Modal"
+import { useState } from "react";
+import { Button } from "../components/Button";
+import { Modal } from "../components/Modal";
 
 export const ModalPage = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const modal = (
+    <Modal
+      onClose={handleClose}
+      actionBar={
+        <Button primary onClick={handleClose}>
+          I Accept
+        </Button>
+      }
+    >
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+    </Modal>
+  );
+
   return (
-    <Modal />
-  )
-}
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        laudantium quae omnis fuga, voluptates, pariatur itaque aliquid,
+        obcaecati ex autem vitae. Asperiores, qui! Unde natus quibusdam enim
+        similique quisquam? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Ipsum quasi molestias nisi quidem ea? Sapiente nisi perferendis,
+        nihil incidunt quidem sed molestias, dicta delectus molestiae explicabo
+        facere dolor distinctio repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Eius, ipsam deleniti non tempora repellat
+        impedit esse voluptate, quisquam quidem rerum, perspiciatis
+      </p>
+
+      <Button onClick={handleClick} primary>
+        Open Modal
+      </Button>
+
+      {showModal && modal}
+    </div>
+  );
+};
