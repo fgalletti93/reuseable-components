3b33fb62092cd3c5f65b9e22c56e1d7df72d745e
Francesca Galletti <francesca.galletti@pipefy.com>
2023-05-23 16:23:57 -0300

    creating hook for sorting logic

--- src/components/SortableTable.jsx
+++ src/components/SortableTable.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
 import { Table } from "./Table";
 import { FaSort, FaSortUp, FaSortDown } from "react-icons/fa";
+import { useSortData } from "../hooks/useSortData";
 
 const getIcons = (label, sortBy, sortOrder) => {
   if (label !== sortBy) {
@@ -17,28 +17,11 @@ const getIcons = (label, sortBy, sortOrder) => {
 };
 
 export const SortableTable = (props) => {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
-
   const { config, data } = props;
-
-  const handleClick = (label) => {
-    if (sortBy && label !== sortBy) {
-      setSortOrder("asc");
-      setSortBy(label);
-      return;
-    }
-    if (sortOrder === null) {
-      setSortOrder("asc");
-      setSortBy(label);
-    } else if (sortOrder === "asc") {
-      setSortOrder("desc");
-      setSortBy(label);
-    } else if (sortOrder === "desc") {
-      setSortOrder(null);
-      setSortBy(null);
-    }
-  };
+  const { handleClick, sortBy, sortOrder, sortedData } = useSortData(
+    data,
+    config
+  );
 
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) return column;
@@ -58,23 +41,6 @@ export const SortableTable = (props) => {
       ),
     };
   });
-
-  let sortedData = data;
-  if (sortOrder && sortBy) {
-    const { sortValue } = config.find((column) => column.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
-
-      const reverseOrder = sortOrder === "asc" ? 1 : -1;
-
-      if (typeof valueA === "string") {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    });
-  }
 
   return <Table {...props} config={updatedConfig} data={sortedData} />;
 };
