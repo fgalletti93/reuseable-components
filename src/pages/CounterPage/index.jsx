24e62b2a4e8dd7cd4f417c4403e45a91f6051e19
Francesca Galletti <francesca.galletti@pipefy.com>
2023-05-25 15:13:41 -0300

    adding immer lib

--- src/pages/CounterPage/index.jsx
+++ src/pages/CounterPage/index.jsx
@@ -1,3 +1,4 @@
+import { produce } from "immer";
 import { useReducer } from "react";
 import { Button } from "../../components/Button";
 import { Panel } from "../../components/Panel";
@@ -11,34 +12,26 @@ import {
 const reducer = (state, action) => {
   switch (action.type) {
     case INCREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count + 1,
-      };
+      state.count = state.count + 1;
+      return;
     case DECREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count - 1,
-      };
+      state.count = state.count - 1;
+      return;
     case SET_VALUE_TO_ADD:
-      return {
-        ...state,
-        valueToAdd: action.payload,
-      };
+      state.valueToAdd = action.payload;
+      return;
     case SUBMIT_VALUE:
-      return {
-        ...state,
-        count: state.count + state.valueToAdd,
-        valueToAdd: 0,
-      };
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
 
     default:
-      return state;
+      return;
   }
 };
 
 export const CounterPage = ({ initialCount }) => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   });
